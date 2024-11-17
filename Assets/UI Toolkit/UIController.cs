cd20656f4656e2d3cd9c3d6415e052fec8517432
275RR-Public <105179611+275RR-Public@users.noreply.github.com>
2024-11-17 17:45:44 -0600

    updated UI

--- Assets/UI Toolkit/UIController.cs
+++ Assets/UI Toolkit/UIController.cs
@@ -10,6 +10,8 @@ public class UIController : MonoBehaviour
     public VisualElement tracker;
     public VisualElement sim;
     public VisualElement play;
+    public VisualElement innerPlay;
+    public Label playLabel;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -24,6 +26,9 @@ public class UIController : MonoBehaviour
         tracker = ui.Q<VisualElement>("TrackContainer");
         sim = ui.Q<VisualElement>("SimContainer");
         play = ui.Q<VisualElement>("PlayContainer");
+        innerPlay = ui.Q<VisualElement>("InnerPlayContainer");
+
+        playLabel = ui.Q<Label>("PlayTitleLbl");
     }
 
     // Update is called once per frame
@@ -36,17 +41,34 @@ public class UIController : MonoBehaviour
     {
         tracker.ToggleInClassList("hide");
         sim.ToggleInClassList("hide");
+        exitBtn.ToggleInClassList("hide");
         
         play.ToggleInClassList("fullscreen");
         play.ToggleInClassList("containPlay");
-
+        
         if(play.ClassListContains("fullscreen"))
         {
             playBtn.text = "Stop";
+            playBtn.style.marginTop = 80;
+            playBtn.style.width = Length.Percent(20);
+
+            innerPlay.style.backgroundColor = new Color(0.1f,0.1f,0.2f,.2f);
+            innerPlay.style.borderLeftWidth = 1;
+            innerPlay.style.borderRightWidth = 1;
+
+            playLabel.style.fontSize = 100;
         }
         else
         {
             playBtn.text = "Play";
+            playBtn.style.marginTop = 40;
+            playBtn.style.width = Length.Percent(60);
+
+            innerPlay.style.backgroundColor = new Color(0,0,0,0);
+            innerPlay.style.borderLeftWidth = 0;
+            innerPlay.style.borderRightWidth = 0;
+
+            playLabel.style.fontSize = 60;
         }
     }
 
